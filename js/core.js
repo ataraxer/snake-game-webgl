c10a14b5591cdc7262f468c382e9b7e999e58ce7
Anton Karamanov <ataraxer@gmail.com>
2014-10-01 23:08:46 +0400

    Enable antialiasing

--- js/core.js
+++ js/core.js
@@ -6,7 +6,9 @@ var start = function(THREE) {
   var aspect = width / height;
   var camera = new THREE.PerspectiveCamera(75, aspect, 0.1, 1000);
 
-  var renderer = new THREE.WebGLRenderer();
+  var renderer = new THREE.WebGLRenderer({
+    antialias: true
+  });
   renderer.setSize(width, height);
   document.body.appendChild( renderer.domElement );
 
